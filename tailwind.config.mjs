8a25e27b9f1d392f12271df10a64141a306a02e0
Sarisha <sarisha.pr@gmail.com>
2025-02-08 15:11:51 +0800

    Add font, color, and container

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -6,12 +7,57 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xs: "390px",
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      xxl: "1400px",
+    },
+    fontFamily: {
+      inter: ["var(--font-inter)"],
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#670000",
+        "gray-dark": "#4A4A4A",
+        "gray-light": "#4A4A4A",
       },
     },
   },
-  plugins: [],
+  // Do not include tailwind default container
+  corePlugins: {
+    container: false,
+  },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        /**
+         * Create custom container
+         * Reference: https://stefvanlooveren.me/blog/custom-container-width-tailwind-css
+         */
+        ".container": {
+          width: "100%",
+          paddingLeft: "15px",
+          paddingRight: "15px",
+          marginLeft: "auto",
+          marginRight: "auto",
+
+          "@screen sm": {
+            maxWidth: "540px",
+          },
+          "@screen md": {
+            maxWidth: "720px",
+          },
+          "@screen lg": {
+            maxWidth: "960px",
+          },
+          "@screen xl": {
+            maxWidth: "79.75vw", // 1692px of 2182px
+          },
+        },
+      });
+    },
+  ],
 };
