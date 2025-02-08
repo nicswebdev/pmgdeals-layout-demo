d89396143b0d7d5eac07e5afe5c53dedc514181d
Sarisha <sarisha.pr@gmail.com>
2025-02-08 16:05:48 +0800

    Replace app router with page router

---
+++ src/pages/_home/page.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <div className="container">
+        <h1>Home</h1>
+      </div>
+    </>
+  );
+}
