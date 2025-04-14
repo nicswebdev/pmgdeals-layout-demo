fa691fb329ccb2e9ce2a5cd7d28ca834f6b04bc2
nicswebdev <nicholaspratama94s@gmail.com>
2025-04-14 09:29:56 +0800

    add gtag script

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,21 +5,37 @@ import {Footer, Header} from "@/components";
 
 import {CurrencyProvider} from "@/context/CurrencyContext";
 import {SessionProvider} from "next-auth/react";
+import Script from "next/script";
 
 const inter = Inter({subsets: ["latin"]});
 const openSans = Open_Sans({subsets: ["latin"]});
 
 function MyApp({Component, pageProps: {session, ...pageProps}}) {
     return (
-        <div className={`${inter.className} ${openSans.className}`}>
-            <SessionProvider session={session}>
-                <CurrencyProvider>
-                    <Header />
-                    <Component {...pageProps} />
-                    <Footer />
-                </CurrencyProvider>
-            </SessionProvider>
-        </div>
+        <>
+            <div className={`${inter.className} ${openSans.className}`}>
+                <SessionProvider session={session}>
+                    <CurrencyProvider>
+                        {/* Google Analytics: G-5LJ54LKV6B */}
+                        <Script
+                            src="https://www.googletagmanager.com/gtag/js?id=G-5LJ54LKV6B"
+                            strategy="afterInteractive"
+                        />
+                        <Script id="ga-gtag" strategy="afterInteractive">
+                            {`
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-5LJ54LKV6B');
+                            `}
+                        </Script>
+                        <Header />
+                        <Component {...pageProps} />
+                        <Footer />
+                    </CurrencyProvider>
+                </SessionProvider>
+            </div>
+        </>
     );
 }
 
