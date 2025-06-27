1ae7693d42a6b90020b6641da1b24d81a164afb6
nicswebdev <nicholaspratama94s@gmail.com>
2025-06-27 20:05:45 +0800

    move gtag head

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -29,6 +29,18 @@ function MyApp({Component, pageProps: {session, ...pageProps}}) {
                             gtag('config', 'G-5LJ54LKV6B');
                             `}
                         </Script>
+
+                        {/* GTM Script for <head> */}
+                        <Script id="gtm-head" strategy="afterInteractive">
+                            {`
+                      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                      })(window,document,'script','dataLayer','GTM-KXH2TWRV');
+                    `}
+                        </Script>
+
                         <Header />
                         <Component {...pageProps} />
                         <Footer />
--- src/pages/thank-you/index.jsx
+++ src/pages/thank-you/index.jsx
@@ -74,16 +74,6 @@ export default function ThankYou() {
                     content={`https://cms.pmgdeals.com/uploads/og-image.png`}
                 />
 
-                {/* GTM Script for <head> */}
-                <Script id="gtm-head" strategy="afterInteractive">
-                    {`
-                      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                      })(window,document,'script','dataLayer','GTM-KXH2TWRV');
-                    `}
-                </Script>
                 {/* Google tag (gtag.js) */}
                 <Script
                     id="gtag-base"
